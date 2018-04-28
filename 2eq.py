d01acd33e6b694194476579f76853821ee4c5689
KIrayamoto <38139345+KIrayamoto@users.noreply.github.com>
2018-04-28 18:48:43 +0800

    Update 2eq.py

--- 2eq.py
+++ 2eq.py
@@ -2,11 +2,16 @@ print('ax^2+bx+c=0')
 a=eval(input('a='))
 b=eval(input('b='))
 c=eval(input('c='))
-d=b2-4ac
-if d >= 0:
-x1=(d0.5-b)/2a
-x2=(-d**0.5-b)/2a
-print('x1=%.4f'% x1)
-print('x2=%.4f'% x2)
-else :
-print('无解')
+d=b**2-4*a*c
+if d > 0:
+	x1=(d**0.5-b)/2*a
+	x2=(-d**0.5-b)/2*a
+	print('x1=%.4f'% x1)
+	print('x2=%.4f'% x2)
+elif d == 0:
+	x=(d**0.5-b)/2*a
+	print('x=%.4f'% x)
+else:
+	print('无解')
+
+
