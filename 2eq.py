a902c83b2db708072eddf057e01e63877465dc7b
KIrayamoto <38139345+KIrayamoto@users.noreply.github.com>
2018-04-28 18:36:33 +0800

    Create 2eq.py

---
+++ 2eq.py
@@ -0,0 +1,12 @@
+print('ax^2+bx+c=0')
+a=eval(input('a='))
+b=eval(input('b='))
+c=eval(input('c='))
+d=b2-4ac
+if d >= 0:
+x1=(d0.5-b)/2a
+x2=(-d**0.5-b)/2a
+print('x1=%.4f'% x1)
+print('x2=%.4f'% x2)
+else :
+print('无解')
