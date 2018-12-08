7035a56f878fb21d7ca17f8ea3f56ffa52b2ed00
KIrayamoto <38139345+KIrayamoto@users.noreply.github.com>
2018-12-09 00:27:48 +0800

    Add files via upload

---
+++ new.py
@@ -0,0 +1,21 @@
+﻿import random
+import math
+a=eval(input('抽号数量:',))
+i=1
+while i<=a :
+	c=random.randint(1,9)
+	b=random.randint(1,9)
+	d=random.randint(1,9)
+	e=random.randint(1,9)
+	f=random.randint(1,9)
+	h=random.randint(1,9)
+	j=random.randint(1,9)
+	g=(math.sin(c)*math.cos(b)*math.tan(d)+math.sin(e)*math.cos(f)*math.tan(h))*j*100000
+	g=str(g)
+	g=g[-3:-1]
+	g=int(float(g))
+	if 0<g<=46:
+		i=i+1
+		print(g)
+
+	
