a1a777d8fb5770a26a51ad37c3a714e305f03385
KIrayamoto <38139345+KIrayamoto@users.noreply.github.com>
2020-01-23 12:32:27 +0800

    Update 质数判断.py
    略微提升计算速度

--- 质数判断.py
+++ 质数判断.py
@@ -2,18 +2,20 @@ a=eval(input('输入一个数:',))
 a0=a
 i=2
 c='1'
-if a <= 1 or not isinstance(a,int):
+if a0 <= 1 or not isinstance(a0,int):
 	print('error')
 else:
-	while i<= a:
+	while i<= a :
+		if i > 2 and i%2==0:
+			i=i+1
 		b=a%i
 		if b == 0:
 			a=a/i
 			c+='x'+str(i)
-			if i == a0 :
+			if i >= a0**0.5 :
 				break
 			i=1
-		i=i+1			
+		i=i+1		
 if i==a0:
 	print('质数')
 else:
