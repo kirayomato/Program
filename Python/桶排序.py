52f8ff8a847ec951b8f89247d67a905c6d51028d
KIrayamoto <38139345+KIrayamoto@users.noreply.github.com>
2020-10-07 23:15:11 +0800

    Update 桶排序.py
    加入对负数的支持，优化小数表现

--- Python/桶排序.py
+++ Python/桶排序.py
@@ -1,23 +1,26 @@
+import math
 L=eval(input('输入一个list:',))
-l0=[]
 l1=[]
 n=0
+m=0
 for a in L:
   if int(a)!=float(a):
     n=max(n,len(str(a))-str(a).index('.')-1)
+  if a < 0:
+    m=min(m,math.floor(a))
 for i in range(len(L)):
-  L[i]*=(10**n)
-while len(l0)<max(L)+1:
-  l0.append(0)
+  L[i]-=m
+  L[i]=int(L[i]*(10**n)+0.5)
+l0=[0]*(max(L)+1)
 for i in L:
   l0[int(i)]+=1
 for x in range(len(l0)):
   if l0[x]!=0:
     i=0
     while i<l0[x]:
-      t=x/(10**n)
+      t=x/(10**n)+m
       if int(t)!=float(t):
-        l1.append(t)
+        l1.append(round(t,n+str(t).index('.')+1))
       else:
         l1.append(int(t))
       i+=1
