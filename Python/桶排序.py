ee7cf1378b4c7edd17c9a817187a6f43d86d19f4
KIrayamoto <38139345+KIrayamoto@users.noreply.github.com>
2020-10-07 22:49:25 +0800

    Update 桶排序.py
    加入对小数的支持

--- Python/桶排序.py
+++ Python/桶排序.py
@@ -1,14 +1,24 @@
 L=eval(input('输入一个list:',))
 l0=[]
 l1=[]
+n=0
+for a in L:
+  if int(a)!=float(a):
+    n=max(n,len(str(a))-str(a).index('.')-1)
+for i in range(len(L)):
+  L[i]*=(10**n)
 while len(l0)<max(L)+1:
   l0.append(0)
 for i in L:
-  l0[i]+=1
+  l0[int(i)]+=1
 for x in range(len(l0)):
   if l0[x]!=0:
     i=0
     while i<l0[x]:
-      l1.append(x)
+      t=x/(10**n)
+      if int(t)!=float(t):
+        l1.append(t)
+      else:
+        l1.append(int(t))
       i+=1
 print(l1)
