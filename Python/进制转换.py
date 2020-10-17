4c4a841e7f15ecf921f09921a70db6f0415f659f
KIrayamoto <38139345+KIrayamoto@users.noreply.github.com>
2020-10-17 21:57:42 +0800

    Create 进制转换.py
    未做错误检测，待优化

---
+++ Python/进制转换.py
@@ -0,0 +1,24 @@
+a=input('需要转换的数：',)
+b=eval(input('原进制：',))
+c=eval(input('目标进制：',))
+dic={10:'A',11:'B',12:'C',13:'D',14:'E',15:'F'}
+DICT = dict(zip(dic.values(), dic.keys()))
+d=0
+n=len(a)
+if b !=10:
+    for i in range(n):
+        if b >10:
+            d+=DICT[a[-i-1]]*b**i
+        else:
+            d+=int(a[-i-1])*b**i
+else:
+    d=int(a)
+r=''
+while d >0:
+    if d%c >9:
+        r=dic[d%c]+r
+        d//=c
+    else:
+        r=str(d%c)+r
+        d//=c
+print('目标数：',r)
