66ac387bb6ae2bb0490a6274193d1d0670c398ff
KIrayamoto <38139345+KIrayamoto@users.noreply.github.com>
2020-11-06 13:20:46 +0800

    Create 快速排序.cpp

---
+++ C++/快速排序.cpp
@@ -0,0 +1,52 @@
+include <iostream>
+#include <vector>
+using namespace std;
+vector<double> vec;
+vector<double> quicksort(vector<double> &v);
+
+int main()
+{
+    cout << "输入数组(以非数字结束)：";
+    double i;
+    while (cin >> i)
+    {
+        vec.push_back(i);
+        if (cin.get() == '\n')
+            break;
+    }
+    vec=quicksort(vec);
+    int num = vec.size();
+    cout << "排序后:";
+    for (int k = 0;k < num;k++)
+    {
+        cout << " " << vec[k];
+    }
+    return 0;
+}
+vector<double> quicksort(vector<double>&v)
+{
+    int n = v.size();
+    if(n <= 1)
+        return v;
+    else
+    {
+        vector<double> small;
+        vector<double> big;
+        int mn = n / 2;
+        double mid = v[mn];
+        for (int k = 0;k < n;k++)
+        {
+            if(k == mn)
+                continue;
+            if (v[k] > mid)
+                big.push_back(v[k]);
+            else
+                small.push_back(v[k]);
+        }
+        small = quicksort(small);
+        big = quicksort(big);
+        small.push_back(v[mn]);
+        small.insert(small.end(),big.begin(),big.end());
+        return small;
+    }
+}
