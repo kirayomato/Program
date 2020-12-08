b949dbef1b128884db4b2be2422acbbcb886d685
KIrayamoto <38139345+KIrayamoto@users.noreply.github.com>
2020-12-08 22:26:58 +0800

    Update 进制转换.py
    升级至2-36进制,增加错误处理

--- Python/进制转换.py
+++ Python/进制转换.py
@@ -1,24 +1,28 @@
-a=input('需要转换的数：',)
-b=int(input('原进制：',))
-c=int(input('目标进制：',))
-dic={10:'A',11:'B',12:'C',13:'D',14:'E',15:'F'}
-DICT = dict(zip(dic.values(), dic.keys()))
-d=0
-n=len(a)
-if b !=10:
-    for i in range(n):
-        if b >10:
-            d+=DICT[a[-i-1]]*b**i
-        else:
-            d+=int(a[-i-1])*b**i
+import sys
+a = input('需要转换的数：',)
+b = int(input('原进制：',))
+c = int(input('目标进制：',))
+if c < 1 or b < 1 or c > 36 or b > 36:
+    print('Error')
 else:
-    d=int(a)
-r=''
-while d >0:
-    if d%c >9:
-        r=dic[d%c]+r
-        d//=c
-    else:
-        r=str(d%c)+r
-        d//=c
-print('目标数：',r)
+    dic = {0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5',
+           6: '6', 7: '7', 8: '8', 9: '9', 10: 'A', 11: 'B',
+           12: 'C', 13: 'D', 14: 'E', 15: 'F', 16: 'G', 17: 'H',
+           18: 'I', 19: 'J', 20: 'K', 21: 'L', 22: 'M', 23: 'N',
+           24: 'O', 25: 'P', 26: 'Q', 27: 'R', 28: 'S', 29: 'T',
+           30: 'U', 31: 'V', 32: 'W', 33: 'X', 34: 'Y', 35: 'Z'}
+    DICT = dict(zip(dic.values(), dic.keys()))
+    d = 0
+    n = len(a)
+    for i in range(n):
+        t = DICT[a[-i - 1]]
+        if t >= b:
+            print('Error')
+            sys.exit()
+        else:
+            d += t * b**i
+    r = ''
+    while d > 0:
+        r = dic[d % c] + r
+        d //= c
+    print('目标数：', r)
