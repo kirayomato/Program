9cebb08a295a7b2d245b446c10c6de8236f2196d
KIrayamoto <38139345+KIrayamoto@users.noreply.github.com>
2021-02-02 02:17:11 +0800

    Create 最长公共子序列.cpp
    二维dp法

---
+++ C++/最长公共子序列.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int n;
+int m1[1005], m2[1005];
+int dp[1005][1005];
+int main()
+{
+	cin >> n;
+	for (int i = 0;i < n;++i)
+		cin >> m1[i];
+	for (int i = 0;i < n;++i)
+		cin >> m2[i];
+	dp[0][0] = 0;
+	dp[1][0] = 0;
+	dp[0][1] = 0;
+	for(int i=1;i<n+1;++i)
+	{
+		for (int j = 1;j < n+1;++j)
+		{
+			if (m1[i-1] == m2[j-1])
+				dp[i][j] = max(dp[i][j - 1], max(dp[i - 1][j - 1] + 1, dp[i - 1][j]));
+			else
+				dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
+		}
+	}
+	cout << dp[n][n];
+	return 0;
+}
