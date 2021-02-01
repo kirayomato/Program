4282c6638b1ea8e0f3d9f92bc35db39313be0047
KIrayamoto <38139345+KIrayamoto@users.noreply.github.com>
2021-02-02 02:20:38 +0800

    Update 最长公共子序列.cpp

--- C++/最长公共子序列.cpp
+++ C++/最长公共子序列.cpp
@@ -18,7 +18,7 @@ int main()
 		for (int j = 1;j < n+1;++j)
 		{
 			if (m1[i-1] == m2[j-1])
-				dp[i][j] = max(dp[i][j - 1], max(dp[i - 1][j - 1] + 1, dp[i - 1][j]));
+				dp[i][j] = dp[i - 1][j - 1] + 1;
 			else
 				dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
 		}
