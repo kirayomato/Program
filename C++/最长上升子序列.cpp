d610438c7f71b0437f65385e3f7c80934f4db220
KIrayamoto <38139345+KIrayamoto@users.noreply.github.com>
2021-02-05 19:17:51 +0800

    Update 最长上升子序列.cpp

--- C++/最长上升子序列.cpp
+++ C++/最长上升子序列.cpp
@@ -1,24 +1,18 @@
+#define INF 999999
 #include<iostream>
 using namespace std;
-int n,ans=1;
+int n, ans = 1;
 int num[2501], dp[2501];
 int main()
 {
-	cin >>n;
+	cin >> n;
 	for (int i = 0;i < n;++i)
 	{
 		cin >> num[i];
-		dp[i] = 1;
+		dp[i] = INF;
 	}
 	for (int i = 0;i < n;++i)
-	{
-		for (int j = 0;j < i;++j)
-		{
-			if (num[i] > num[j])
-				dp[i] = max(dp[i], dp[j] + 1);
-		}
-		ans = max(ans, dp[i]);
-	}
-	cout << ans;
+		*lower_bound(dp, dp+n, num[i]) = num[i];
+	cout << lower_bound(dp, dp + n, INF)-dp;
 	return 0;
 }
