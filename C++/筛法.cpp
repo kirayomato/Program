e5aa48d918e5e98bd5fd92cade6b82c38cf02b31
KIrayamoto <38139345+KIrayamoto@users.noreply.github.com>
2021-03-07 14:12:06 +0800

    Update 筛法.cpp

--- C++/筛法.cpp
+++ C++/筛法.cpp
@@ -2,28 +2,22 @@
 #include <vector>
 using namespace std;
 vector<int> vec;
-bool prime(int);
-
 int main()
 {
-    int i, t,num;
+    int i, t, num;
     cout << "结束值：";
     cin >> num;
     for (i = 0;i <= num;i++)
     {
         vec.push_back(i);
     }
-    for (i = 0;i <= num;i++)
+    for (i = 2;i <= num;i++)
     {
-        if (prime(vec[i]))
+        if (vec[i]!=0)
         {
-            int *max = &vec.back();
-            while (*max == 0)
-                max -= 1;
-            int n = *max / vec[i];
+            int n = num / vec[i];
             for (t = 2;t <= n;t++)
                 vec[vec[i] * t] = 0;
-
         }
     }
     for (vector<int>::iterator it = vec.begin();it != vec.end();)
@@ -35,21 +29,7 @@ int main()
     }
     for (i = 1;i < vec.size();i++)
     {
-        cout<<vec[i]<<" ";
+        cout << vec[i] << " ";
     }
     return 0;
-}
-bool prime(int x)
-{
-    if (x <= 1)
-        return false;
-    if (x == 2)
-        return true;
-    for (int k = 2;k < x;k++)
-    {
-        if (x % k == 0)
-            return false;
-        else  if (x == k - 1)
-            return true;
-    }
 }
