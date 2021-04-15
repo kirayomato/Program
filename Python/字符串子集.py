baa2a1efccd5c45a12186b1fc8fed24de190d931
KIrayamoto <38139345+KIrayamoto@users.noreply.github.com>
2021-04-15 21:18:22 +0800

    Update 字符串子集.py

--- Python/字符串子集.py
+++ Python/字符串子集.py
@@ -3,20 +3,11 @@ a = input()
 
 def fset(x):
     t = len(x)
-    l1 = []
     l = []
-    for j in range(1,2**t):
-        s = ''
+    for j in range(1, 2**t):
+        s = ""
         for i in range(t):
             if j & 2**i:
-                s += '1'
-            else:
-                s += '0'
-        l1.append(s)
-    for j in l1:
-        s = ''
-        for i in range(len(j)):
-            if j[i] == '1':
                 s += a[i]
         l.append(s)
     return l
