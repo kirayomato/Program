71c6d257c9f08a4230e863671df052bac82547d6
KIrayamoto <38139345+KIrayamoto@users.noreply.github.com>
2021-04-16 19:03:29 +0800

    Update 线段树rmq.py

--- Python/线段树rmq.py
+++ Python/线段树rmq.py
@@ -1,7 +1,8 @@
 from math import log
 from math import ceil
 a = list(map(eval, input().split()))
-
+[x, y] = list(map(eval, input().split()))
+[i, j] = list(map(eval, input().split()))
 t = len(a)
 b = [float('inf')] * (2 ** (ceil(log(t, 2) + 1)) - 1)
 
@@ -15,16 +16,55 @@ def lrmq(list_a, list_b, pos, left, right):
         list_b[pos] = min(list_b[2 * pos + 1], list_b[2 * pos + 2])
 
 
+def search(list_b, left, right, i, j, pos):
+    if j < left or i > right:
+        return float('inf')
+    if i <= left and j >= right:
+        return list_b[pos]
+    mid = (left + right) // 2
+    if i > mid:
+        return search(list_b, mid + 1, right, i, j, 2 * pos + 2)
+    elif j <= mid:
+        return search(list_b, left, mid, i, j, 2 * pos + 1)
+    else:
+        return min(search(list_b, mid + 1, right, i, j, 2 * pos + 2),
+                   search(list_b, left, mid, i, j, 2 * pos + 1))
+
+
+def query(list_a, list_b, left, right):
+    return search(list_b, 0, len(list_a) - 1, left, right, 0)
+
+
+def update(list_a, list_b, pos, value):
+    posb = 0
+    left = 0
+    right = len(list_a) - 1
+    while right > left:
+        mid = (left + right) // 2
+        if pos > mid:
+            left = mid + 1
+            posb = 2 * posb + 2
+        else:
+            right = mid
+            posb = 2 * posb + 1
+    list_b[posb] = value
+    while posb > 0:
+        posb = int((posb - 1) / 2)
+        list_b[posb] = min(list_b[2 * posb + 1], list_b[2 * posb + 2])
+
+
 lrmq(a, b, 0, 0, t - 1)
+update(a, b, i - 1, j)
+print(query(a, b, x - 1, y - 1))
 n = int(log(len(b), 2)) + 1
 for i in range(n):
-    t = 2 ** i - 1
-    print(" " * (2**(n - i - 1) - 1), end="")
-    for j in range(2**i):
-        if j + t == len(b):
-            break
-        if b[j + t] == float('inf'):
-            print(" ", end=" " * (2 ** (n - i) - 1))
-        else:
-            print(b[j + t], end=" " * (2**(n - i) - 1))
+        t = 2 ** i - 1
+        print(" " * (2**(n - i - 1) - 1), end="")
+        for j in range(2**i):
+            if j + t == len(b):
+                break
+            if b[j + t] == float('inf'):
+                print(" ", end=" " * (2 ** (n - i) - 1))
+            else:
+                print(b[j + t], end=" " * (2**(n - i) - 1))
     print()
