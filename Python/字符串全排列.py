1951bc3b9aacd24649878f8c749b524759f6ab3b
KIrayamoto <38139345+KIrayamoto@users.noreply.github.com>
2021-04-21 20:08:10 +0800

    Update 字符串全排列.py

--- Python/字符串全排列.py
+++ Python/字符串全排列.py
@@ -1,5 +1,6 @@
 a = input()
 
+
 def frange(x):
     t = len(x)
     if t == 1:
@@ -7,11 +8,10 @@ def frange(x):
     else:
         l = []
         for i in range(t):
-            p = x[:i] + x[i + 1:]
-            lt = [x[i]]
-            lp = frange(p)
-            l += [i + j for i in lt for j in lp]
+            lp = frange(x[:i] + x[i + 1:])
+            l += [x[i] + j for j in lp]
         return l
+
 
 for x in sorted(set(frange(a))):
     print(x, end=' ')
