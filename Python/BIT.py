b371f6dbd2f1aeb88b7ab8f4e760d6547c11f175
KIrayamoto <38139345+KIrayamoto@users.noreply.github.com>
2021-04-28 19:45:37 +0800

    Update BIT.py

--- Python/BIT.py
+++ Python/BIT.py
@@ -1,5 +1,8 @@
 from math import log
+
 a = list(map(eval, input().split()))
+[i, j] = list(map(eval, input().split()))  # query [i,j]
+[pos, val] = list(map(eval, input().split()))  # add val to a[pos]
 
 
 def bit(list):
@@ -13,9 +16,28 @@ def bit(list):
             b[j] += b[j - 1]
             i -= 2
             for k in range(t - 1):
-                i -= 2**k
+                i -= 2 ** k
                 b[j] += b[i]
     return b
+
+
+def query(list, i, j):
+    i -= 1
+    sum0 = sum1 = 0
+    while i > 0:
+        sum0 += list[i - 1]
+        i -= i & -i
+    while j > 0:
+        sum1 += list[j - 1]
+        j -= j & -j
+    return sum1 - sum0
+
+
+def add(list, pos, val):
+    t = len(list)
+    while pos <= t:
+        list[pos - 1] += val
+        pos += pos & -pos
 
 
 def printbit(list):
@@ -26,7 +48,7 @@ def printbit(list):
         j = 0
         lp = 0
         while j < t:
-            j += 2**i
+            j += 2 ** i
             if book[j - 1]:
                 print("%s%d" % (" " * 2 * (j - 1 - lp), list[j - 1]), end=' ')
                 book[j - 1] = False
@@ -34,4 +56,7 @@ def printbit(list):
         print()
 
 
-printbit(bit(a))
+a = bit(a)
+add(a, pos, val)
+print(query(a, i, j))
+printbit(a)
