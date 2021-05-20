629fe3d282cda8b7ed9a559a1e1a25826bdcfaad
KIrayamoto <38139345+KIrayamoto@users.noreply.github.com>
2021-05-20 11:21:24 +0800

    Create 复杂优先队列.cpp

---
+++ C++/复杂优先队列.cpp
@@ -0,0 +1,23 @@
+#include<queue>
+using namespace std;
+//pair 
+typedef pair<int, int> P;
+struct cmp
+{
+    bool operator()(const P p1, const P p2)
+    {
+        return p1.second > p2.second; //小顶堆
+    }
+};
+priority_queue<P, vector<P>, cmp> que
+
+//结构体
+struct node
+{
+    int value;
+};
+bool operator<(const node &a, const node &b)
+{
+    return a.value > b.value;//小顶堆。
+}
+priority_queue<node> p;
