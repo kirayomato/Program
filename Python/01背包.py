03a3a68acfd2a9ebaf4a56546b6e106759790e53
KIrayamoto <38139345+KIrayamoto@users.noreply.github.com>
2021-06-03 21:14:08 +0800

    Update 01背包.py

--- Python/01背包.py
+++ Python/01背包.py
@@ -10,6 +10,6 @@ for i in range(1, t + 1):
         if we[i - 1] > j:
             choose[i][j] = choose[i - 1][j]
         else:
-            choose[i][j] = max(choose[i - 1][j - we[i - 1]] +
-                               pr[i - 1], choose[i - 1][j])
+            choose[i][j] = max(choose[i - 1][j - we[i - 1]] + pr[i - 1], choose[i - 1][j])
+   #完全背包 choose[i][j] = max(choose[i][j - we[i - 1]] + pr[i - 1], choose[i - 1][j]) 
 print("max value:", choose[-1][-1])
