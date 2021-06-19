50ac28755783bf1a4cf676203c52f85a972fabb6
KIrayamoto <38139345+KIrayamoto@users.noreply.github.com>
2021-06-19 19:55:38 +0800

    Update 字符串转数组.cpp

--- C++/字符串转数组.cpp
+++ C++/字符串转数组.cpp
@@ -1,17 +1,14 @@
-#include<iostream>
 #include<vector>
 #include<sstream>
-using namespace std;
 vector<int> vectorin(string s)
 {
 	stringstream ss;
 	ss << s;
 	char a;
 	vector<int> t;
-	while (ss>>a)
+	while (ss >> a)
 	{
-		const char* p = &a;
-		int n = atoi(p);
+		int n = a-'0';
 		t.push_back(n);
 	}
 	ss.clear();
