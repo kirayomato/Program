59ab51edd482f8d2ab5b8c704633d3636d65e9ad
KIrayamoto <38139345+KIrayamoto@users.noreply.github.com>
2021-06-19 20:15:02 +0800

    Update 向量高精度乘法和加法.cpp

--- C++/向量高精度乘法和加法.cpp
+++ C++/向量高精度乘法和加法.cpp
@@ -1,37 +1,3 @@
-vector<int> vecmul(vector<int> x, vector<int> b)
-{
-	while (x.size() < b.size())
-		x.insert(x.begin(), 0);
-	while (x.size() > b.size())
-		b.insert(b.begin(), 0);
-	vector<int>::reverse_iterator ita;
-	vector<int>::reverse_iterator itb;
-	vector<int> t;
-	for (itb = b.rbegin(); itb != b.rend(); ++itb)
-	{
-		int t1, t2 = 0;
-		vector<int>a = x;
-		for (ita = a.rbegin(); ita != a.rend();++ita)
-		{
-			t1 = *ita * *itb + t2;
-			if (t1 >= 10)
-			{
-				t2 = t1 / 10;
-				t1 %= 10;
-			}
-			else
-				t2 = 0;
-			*ita = t1;	
-		}
-		if (t2 > 0)
-			a.insert(a.begin(), t2);
-		t = vecadd(t, a);
-		x.push_back(0);
-	}
-	while (*t.begin() == 0)
-		t.erase(t.begin());
-	return t;
-}
 vector<int> vecadd(vector<int> a, vector<int> b)
 {
 	while (a.size() < b.size())
@@ -55,5 +21,41 @@ vector<int> vecadd(vector<int> a, vector<int> b)
 	}
 	if (t2 > 0)
 		a.insert(a.begin(), t2);
+	while (*a.begin() == 0&&a.size()>1)
+		a.erase(a.begin());
 	return a;
+}
+vector<int> vecmul(vector<int> x, vector<int> b)
+{
+	while (x.size() < b.size())
+		x.insert(x.begin(), 0);
+	while (x.size() > b.size())
+		b.insert(b.begin(), 0);
+	vector<int>::reverse_iterator ita;
+	vector<int>::reverse_iterator itb;
+	vector<int> t;
+	for (itb = b.rbegin(); itb != b.rend(); ++itb)
+	{
+		int t1, t2 = 0;
+		vector<int>a = x;
+		for (ita = a.rbegin(); ita != a.rend();++ita)
+		{
+			t1 = *ita * *itb + t2;
+			if (t1 >= 10)
+			{
+				t2 = t1 / 10;
+				t1 %= 10;
+			}
+			else
+				t2 = 0;
+			*ita = t1;
+		}
+		if (t2 > 0)
+			a.insert(a.begin(), t2);
+		t = vecadd(t, a);
+		x.push_back(0);
+	}
+	while (*t.begin() == 0 && t.size() > 1)
+		t.erase(t.begin());
+	return t;
 }
