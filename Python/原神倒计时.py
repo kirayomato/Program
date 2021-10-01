98bf46208e4cb39c7b2fc57483f94135a2bf5da3
KIrayamoto <38139345+KIrayamoto@users.noreply.github.com>
2021-10-01 15:09:01 +0800

    Update 原神倒计时.py
    使用apscheduler重写

--- Python/原神倒计时.py
+++ Python/原神倒计时.py
@@ -1,18 +1,24 @@
 import win32api
-import time
-a = eval(input("hour:",) or "9") #default:9
-b = eval(input("minute:",) or "0") #default:0
+from datetime import datetime, timedelta
+from apscheduler.schedulers.blocking import BlockingScheduler
+a = eval(input("hour:",) or "9")  # default:9
+b = eval(input("minute:",) or "0")  # default:0
 t = 3600 * a + 60 * b
-print("end time:", time.asctime(time.localtime(time.time() + t)))
-print("Remaining time:%02d:%02d:%02d" % (a, b, 0), end='')
-while t > 0:
-    time.sleep(1)
-    t -= 1
-    h = t // 3600
-    m = (t - 3600 * h) // 60
-    s = t - 3600 * h - 60 * m
-    print("\rRemaining time:%02d:%02d:%02d" % (h, m, s), end='')
-print("\rTime Out!                    ")
-win32api.ShellExecute(0, 'open',
-                      r"F:\Game\Genshin Impact\Genshin Impact Game\YuanShen.exe" #Your Genshin address
-                      , '', '', 1)
+dt = datetime.now() + timedelta(seconds=t)
+print("end time:", dt.strftime("%m-%d %H:%M:%S"))
+
+
+def print_time():
+    td = timedelta(seconds=(dt - datetime.now()).seconds)
+    print("\rRemaining time:{}".format(td), end='')
+    if td.seconds == 0:
+        scheduler.shutdown(wait=False)
+        print("\rTime Out!                    ")
+        win32api.ShellExecute(0, 'open',
+                              r"F:\Game\Genshin Impact\Genshin Impact Game\YuanShen.exe",  # Your Genshin address
+                              '', '', 1)
+
+
+scheduler = BlockingScheduler()
+scheduler.add_job(print_time, 'interval', seconds=1)
+scheduler.start()
