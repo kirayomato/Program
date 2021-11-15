24095bef8081a8262832925752a17e005e6b615b
KIrayamoto <38139345+KIrayamoto@users.noreply.github.com>
2021-11-15 09:58:32 +0800

    Update 原神倒计时.py

--- Python/原神倒计时.py
+++ Python/原神倒计时.py
@@ -1,6 +1,10 @@
 from win32api import ShellExecute
 from datetime import datetime, timedelta
 from apscheduler.schedulers.blocking import BlockingScheduler
+from time import sleep
+from warnings import filterwarnings
+import ctypes
+filterwarnings("ignore")
 address = "F:/Game/Genshin Impact/"  # Your Genshin address
 with open(address + "travel.txt", 'r') as f:
     try:
@@ -27,29 +31,32 @@ print("end time:", dt.strftime("%Y-%m-%d %H:%M:%S"))
 
 def start_genshin():
     try:
-        ShellExecute(0, 'open', address + "Genshin Impact Game/YuanShen.exe", '', '', 1)
+        # ShellExecute(0, 'open', address + "Genshin Impact Game/YuanShen.exe", '', '', 1)
+        ShellExecute(0, 'open', "C:/Users/AMD PC/Desktop/genshin.bat", '', '', 1)
         with open(address + "travel.txt", 'w') as f:
             f.write("False\n")
-    except BaseException:  # once open failed, will not continue next time
+    except BaseException:
         with open(address + "travel.txt", 'a') as f:
             f.write('True')
 
 
 def print_time():
     td = timedelta(seconds=(dt - datetime.now()).seconds)
-    print("\r{:23}".format("Remaining time:" + str(td)), end='')
+    print("\rRemaining time:{}".format(td), end='')
     if td.seconds <= 0:
         scheduler.shutdown(wait=False)
-        print("\r{:23}".format("Time Out!"))
+        print("\rTime Out!                    ")
         start_genshin()
 
 
 if dt > datetime.now():
     scheduler = BlockingScheduler()
+    sleep(5)  # 五秒后自动最小化窗口
+    ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 6)
     scheduler.add_job(print_time, 'interval', seconds=1)
     try:
         scheduler.start()
-    except KeyboardInterrupt:  # ctrl+c to break
+    except KeyboardInterrupt:
         scheduler.shutdown(wait=False)
         with open(address + "travel.txt", 'a') as f:
             f.write('True')
