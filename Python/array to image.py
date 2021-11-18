2b0fea9aff437a3467a6aab0d401760645709d1a
KIrayamoto <38139345+KIrayamoto@users.noreply.github.com>
2021-11-18 21:18:13 +0800

    Update array to image.py

--- Python/array to image.py
+++ Python/array to image.py
@@ -1,14 +1,18 @@
 import numpy as np
 from PIL import Image
+w, h = 512, 512
 # 单通道
-# img = np.zeros((256, 256), dtype="uint8")
+# img = np.zeros((w, h), dtype="uint8")
 # 三通道
-img = np.zeros((256, 256, 3), dtype="uint8")
-for i in range(256):
-    for j in range(256):
-        img[j, i, 0] = np.uint8(i)
-        img[j, i, 1] = np.uint8(i)
-        img[j, i, 2] = np.uint8(i)
+img = np.zeros((w, h, 3), dtype="uint8")
+for i in range(w):
+    for j in range(h):
+        # 单通道
+        # img[j, i] = np.uint8(i/2)
+        # 三通道
+        img[j, i, 0] = np.uint8(i/2)
+        img[j, i, 1] = np.uint8(i/2)
+        img[j, i, 2] = np.uint8(i/2)
 
 im = Image.fromarray(img)
 im.show()
