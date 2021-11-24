c680d9ad061868cb83e97b3f8186f0d0d4c48a3c
KIrayamoto <38139345+KIrayamoto@users.noreply.github.com>
2021-11-24 20:11:00 +0800

    Create py打包图标.py

---
+++ Python/py打包图标.py
@@ -0,0 +1,19 @@
+from base64 import b64encode
+with open("icon.py","w") as f:
+    f.write('class Icon(object):\n')
+    f.write('\tdef __init__(self):\n')
+    f.write("\t\tself.img='")
+with open("icon.ico","rb") as i:
+    b64str = b64encode(i.read())
+    with open("icon.py","ab+") as f:
+        f.write(b64str)
+with open("icon.py","a") as f:
+    f.write("'")
+    
+#Then in main.py
+from base64 import b64encode
+from icon import Icon
+with open('tmp.ico', 'wb') as tmp:
+    tmp.write(b64decode(Icon().img))
+app.setWindowIcon(QIcon('tmp.ico'))
+remove('tmp.ico')
