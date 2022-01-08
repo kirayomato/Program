cb6beaab6739aa4f8cdd60b76cad124657e39bce
KIrayamoto <38139345+KIrayamoto@users.noreply.github.com>
2022-01-09 00:54:49 +0800

    Update 傅里叶变换.py

--- Python/傅里叶变换.py
+++ Python/傅里叶变换.py
@@ -5,17 +5,17 @@ from math import cos, sin
 
 def ft(x, y):
     M = 1000  # 傅里叶级数个数
-    wm = 5  # (pi)
+    wm = 5  
     N = len(x)
-    t2 = x[-1]-x[0]
+    t = x[-1]-x[0]
     y1 = [0]*(2*M+1)
     y2 = [0]*(2*M+1)
     lk = range(-M, M+1)
     w0 = wm/M
     for k in lk:
         for i in range(N):
-            y1[k+M] -= y[i]*sin(k*x[i]*w0)*t2/N
-            y2[k+M] += y[i]*cos(k*x[i]*w0)*t2/N
+            y1[k+M] -= y[i]*sin(k*x[i]*w0)*t/N
+            y2[k+M] += y[i]*cos(k*x[i]*w0)*t/N
     lx = np.linspace(-wm, wm, 2*M+1)
     plt.plot(lx, y2, color='r', label='Real')
     plt.plot(lx, y1, color='b', label='Imaginary')
