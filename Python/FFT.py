41649c620a4fea5b0c48b63e852eeb772dfc5147
KIrayamoto <38139345+KIrayamoto@users.noreply.github.com>
2022-01-08 22:23:44 +0800

    Update FFT.py
    分别显示实部和虚部

--- Python/FFT.py
+++ Python/FFT.py
@@ -4,18 +4,23 @@ import matplotlib.pyplot as plt
 
 def fft(x, y):
     M = 1000  # 傅里叶级数个数
-    wm = 10  # (pi)
+    wm = 5  # (pi)
     N = len(x)
     t2 = x[-1]-x[0]
+    y1 = np.zeros(2*M+1)
     y2 = np.zeros(2*M+1)
     lk = range(-M, M+1)
     w0 = wm/M
     for k in lk:
         for i in range(N):
-            y2[k+M] += np.real(y[i]*np.exp(-1j*k*x[i]*w0))
+            y1[k+M] -= y[i]*np.sin(k*x[i]*w0)
+            y2[k+M] += y[i]*np.cos(k*x[i]*w0)
+    y1 /= N/t2
     y2 /= N/t2
     lx = np.linspace(-wm, wm, 2*M+1)
-    plt.plot(lx, y2)
+    plt.plot(lx, y2, color='r', label='Real')
+    plt.plot(lx, y1, color='b', label='Imaginary')
+    plt.legend(loc='best')
     plt.show()
 
 
@@ -23,5 +28,5 @@ t = 10
 N = 1000
 x = np.linspace(-t, t, N+1)
 y = np.piecewise(x, [x < 0, x >= 0], [0, lambda x: np.exp(-x)])
-y1 = 1+2*np.cos(5*x)+np.cos(7*x)
+y1 = np.cos(x)+np.sin(3*x)
 fft(x, y1)
