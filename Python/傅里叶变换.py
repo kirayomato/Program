eee060ea28270d0d73865cab9e6ab536f0ada664
KIrayamoto <38139345+KIrayamoto@users.noreply.github.com>
2022-01-09 17:59:16 +0800

    Update 傅里叶变换.py

--- Python/傅里叶变换.py
+++ Python/傅里叶变换.py
@@ -1,11 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from math import cos, sin
 
 
 def ft(x, y):
     M = 1000  # 傅里叶采样个数
-    wm = 5    # 最大ω值
+    wm = 10    # 最大ω值
     N = len(x)
     t = x[-1]-x[0]
     y1 = [0]*(2*M+1)
@@ -13,9 +12,8 @@ def ft(x, y):
     lk = range(-M, M+1)
     w0 = wm/M
     for k in lk:
-        for i in range(N):
-            y1[k+M] -= y[i]*sin(k*x[i]*w0)*t/N
-            y2[k+M] += y[i]*cos(k*x[i]*w0)*t/N
+        y1[k+M] = -np.dot(y, np.sin(k*x*w0))*t/N
+        y2[k+M] = np.dot(y, np.cos(k*x*w0))*t/N
     lx = np.linspace(-wm, wm, 2*M+1)
     plt.plot(lx, y2, color='r', label='Real')
     plt.plot(lx, y1, color='b', label='Imaginary')
@@ -36,4 +34,5 @@ x = np.linspace(-t, t, N+1)
 y0 = np.piecewise(x, [abs(x) <= 1/2, abs(x) > 1], [1, 0])
 y = np.piecewise(x, [x < 0, x >= 0], [0, lambda x: np.exp(-x)])
 y1 = np.cos(x)+np.sin(3*x)
-ft(x, y1)
+y2 = np.piecewise(x, [abs(x) < 1, abs(x) >= 1], [lambda x:x, 0])
+ft(x, y)
