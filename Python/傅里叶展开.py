226dde49534072193b14ab2f1a50d6203adbc80a
KIrayamoto <38139345+KIrayamoto@users.noreply.github.com>
2022-01-09 18:07:28 +0800

    Update 傅里叶展开.py

--- Python/傅里叶展开.py
+++ Python/傅里叶展开.py
@@ -10,9 +10,8 @@ def fe(x, y, w0):  # w0为基频
     y2 = [0]*(2*M+1)
     lk = range(-M, M+1)
     for k in lk:
-        for i in range(N):
-            y1[k+M] -= y[i]*sin(k*x[i]*w0)/N
-            y2[k+M] += y[i]*cos(k*x[i]*w0)/N
+        y1[k+M] = -np.dot(y, np.sin(k*x*w0))/N
+        y2[k+M] = np.dot(y, np.cos(k*x*w0))/N
     plt.bar(np.arange(-M, M+1), y2, color='r', width=0.5, label='Real')
     plt.bar(np.arange(-M, M+1)+0.5, y1, color='b', width=0.5, label='Imaginary')
     plt.legend(loc='best')
@@ -27,7 +26,7 @@ def fe(x, y, w0):  # w0为基频
 
 
 t = 10
-N = 1000
+N = 10000
 x = np.linspace(-t, t, N+1)
 y = np.cos(x)+np.sin(3*x)
 fe(x, y, 1)
