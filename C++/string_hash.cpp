b9612c1a32dd8fd1653f721fad4473bbf23e4b77
KIrayamoto <38139345+KIrayamoto@users.noreply.github.com>
2022-01-18 02:06:29 +0800

    Update string_hash.cpp

--- C++/string_hash.cpp
+++ C++/string_hash.cpp
@@ -1,33 +1,27 @@
-static constexpr int m = 998244353;
+static constexpr int BASE = 73;
 static constexpr int mod = 1e9 + 7;
 typedef long long ll;
-vector<ll> ans;
-vector<ll> string_hash(string s,int m)
+vector<ll> string_hash(string s)
 {
     n = s.size();
     vector<ll> has(n+1);
     F(0, i, n)
-        has[i+1] = (s[i] + (has[i] * m) % mod) % mod;
+        has[i+1] = (s[i] + (has[i] * BASE) % mod) % mod;
     return has;
 }
-ll qpow(ll x, ll y)
+vector<ll> init(int n)
 {
-    ll t = 1;
-    if (y == 0)
-        return 1;
-    while (y > 0)
-    {
-        if (y & 1)
-            t = (t * x) % mod;
-        x = (x * x) % mod;
-        y >>= 1;
-    }
-    return t % mod;
+    vector<ll> pe;
+    pe.resize(N);
+    pe[0] = 1;
+    F(1, i, n)
+        pe[i] = pe[i - 1] * BASE % mod;
+    return pe;
 }
-ll shash(int x,int y) //index start from 0
+ll shash(int x, int y, const vector<ll>& has)
 {
     if (x == 0)
-        return ans[y+1] ;
+        return has[y + 1];
     else
-        return (ans[y+1] - (ans[x] * qpow(m, y - x + 1))%mod+mod)%mod;
+        return (has[y + 1] - (has[x] * pe[y - x + 1]) % mod + mod) % mod;
 }
