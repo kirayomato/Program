ed0e8ea5e3c654dea583e6f6269b7437b4cb3530
KIrayamoto <38139345+KIrayamoto@users.noreply.github.com>
2022-03-07 12:58:29 +0800

    Update 进制转换.py

--- Python/进制转换.py
+++ Python/进制转换.py
@@ -1,4 +1,4 @@
-def base_convert(a, b, c, pre=1e-3):
+def base_convert(ori_num, ori_base, tar_base, pre=1):
     dic = {0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5',
            6: '6', 7: '7', 8: '8', 9: '9', 10: 'A', 11: 'B',
            12: 'C', 13: 'D', 14: 'E', 15: 'F', 16: 'G', 17: 'H',
@@ -6,30 +6,39 @@ def base_convert(a, b, c, pre=1e-3):
            24: 'O', 25: 'P', 26: 'Q', 27: 'R', 28: 'S', 29: 'T',
            30: 'U', 31: 'V', 32: 'W', 33: 'X', 34: 'Y', 35: 'Z'}
     DICT = dict(zip(dic.values(), dic.keys()))
-    if '.' not in a:
-        a += '.'
-    a1, a2 = str(a.upper()).split('.')
-    n, m = len(a1), len(a2)
+    if '.' not in ori_num:
+        ori_num += '.'
+    a1, a2 = ori_num.upper().split('.')
     r1 = r2 = ''
-    if a1 != '':
-        d = 0
-        for i in range(n):
-            t = DICT[a1[-i - 1]]
-            d += t * b**i
-        while d > 0:
-            r1 = dic[d % c] + r1
-            d //= c
-    else:
-        r1 = '0'
+    flag = False
+    if a1 == '':
+        a1 = '0'
+    if a1[0] == '-':
+        flag = True
+        a1 = a1[1:]
+    n, m = len(a1), len(a2)
+    d = 0
+    for i in range(n):
+        t = DICT[a1[-i - 1]]
+        d += t * ori_base**i
+    while d >= 0:
+        r1 = dic[d % tar_base] + r1
+        d //= tar_base
+        if d == 0:
+            break
+    if flag:
+        r1 = '-'+r1
     if a2 == '':
         return r1
     d = 0
     for i in range(m):
         t = DICT[a2[i]]
-        d += t * b**(-i-1)
+        d += t * ori_base**(-i-1)
+    if pre == 1:
+        pre = 10**(-m-1)
     while d > pre:
-        d *= c
+        d *= tar_base
         r2 += dic[int(d)]
         d -= int(d)
-        pre *= c
+        pre *= tar_base
     return r1+"."+r2
