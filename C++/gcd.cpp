520f731db991a546cf6f4818d6b0347602823518
KIrayamoto <38139345+KIrayamoto@users.noreply.github.com>
2022-04-10 12:47:40 +0800

    Update gcd.cpp

--- C++/gcd.cpp
+++ C++/gcd.cpp
@@ -2,8 +2,7 @@ int gcd(int a, int b)
 {
     return b > 0 ? gcd(b, a % b) : a;
 }
-// 求逆元：a,b互质,y任意,x为逆元
-// ax+by=gcd(a,b)=1 -> ax%b=1
+//ax+by=gcd(a,b)=1
 void exgcd(int a, int b, int& x, int& y) 
 {
     if (b == 0) {
@@ -12,4 +11,14 @@ void exgcd(int a, int b, int& x, int& y)
     }
     exgcd(b, a % b, y, x);
     y -= a / b * x;
+}
+int number_theoretic_reciprocal(int a, int b)
+{
+    int x = 0,y=0;
+    // 求逆元：a,b互质,y任意,x为逆元
+    // ax+by=gcd(a,b)=1 -> ax%b=1
+    exgcd(a, b, x, y);
+    int t = b / gcd(a, b);
+    x = (x % t + t) % t;
+    return x;
 }
