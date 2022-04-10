1e1d52b6c08e34861b065b8148c58f31ce9e5b72
KIrayamoto <38139345+KIrayamoto@users.noreply.github.com>
2022-04-10 13:47:25 +0800

    Update and rename gcd.cpp to 中国剩余定理.cpp

---
+++ C++/中国剩余定理.cpp
@@ -0,0 +1,38 @@
+ll gcd(ll a, ll b) 
+{
+    return b > 0 ? gcd(b, a % b) : a;
+}
+//ax+by=gcd(a,b)=1
+void exgcd(ll a, ll b, ll& x, ll& y) 
+{
+    if (b == 0) {
+        x = 1, y = 0;
+        return;
+    }
+    exgcd(b, a % b, y, x);
+    y -= a / b * x;
+}
+ll number_theoretic_reciprocal(ll a, ll b)
+{
+    ll x = 0,y=0;
+    // 求逆元：a,b互质,y任意,x为逆元
+    // ax+by=gcd(a,b)=1 -> ax%b=1
+    exgcd(a, b, x, y);
+    ll t = b / gcd(a, b);
+    x = (x % t + t) % t;
+    return x;
+}
+//要求两两互质
+ll crt(unordered_map<int, int>mp) 
+{
+    ll nn = 1, ans = 0;
+    FA(i, mp)
+        nn *= i.first;
+    FA(i, mp)
+    {
+        ll m1 = nn / i.first;
+        int m2 = number_theoretic_reciprocal(m1, i.first);
+        ans = (ans + i.second * m1 * m2 % nn) % nn;
+    }
+    return ans;
+}
