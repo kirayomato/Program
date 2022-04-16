15c38bddfe407538452a54a85c665685749d0406
KIrayamoto <38139345+KIrayamoto@users.noreply.github.com>
2022-04-16 13:16:33 +0800

    Create lca.cpp

---
+++ C++/lca.cpp
@@ -0,0 +1,68 @@
+vector<vector<int>>G;
+vector<int>mp;
+int fa[N][31];
+void bfs()
+{
+    F(0, i, n)
+        fill(fa[i], fa[i] + 31, -1);
+    bool vis[N];
+    queue<int> q;
+    q.emplace(0);
+    vis[0] = 1;
+    mp[0] = 0;
+    WE(q)
+    {
+        int i = FR(q);
+        FA(j, G[i])
+        {
+            if (!vis[j])
+            {
+                q.emplace(j);
+                vis[j] = 1;
+                mp[j] = mp[i] + 1;
+                fa[j][0] = i;
+                int t = i;
+                F(0, u, 31)
+                {
+                    if (fa[t][u] != -1)
+                    {
+                        fa[j][u + 1] = fa[t][u];
+                        t = fa[t][u];
+                    }
+                    else
+                        break;
+                }
+            }
+        }
+    }
+}
+int lca(int x, int y)
+{
+    if (mp[x] < mp[y])
+        swap(x, y);
+    int tmp = mp[x] - mp[y];
+    _F(30, i, 0)
+    {
+        if (tmp & (1 << i))
+        {
+            x = fa[x][i];
+            tmp ^= (1 << i);
+        }
+        if(!tmp)
+            break
+    }
+    while (x != y)
+    {
+        _F(30, i, 0)
+        {
+            if (fa[x][i] != fa[y][i] && fa[x][i] != -1 && fa[y][i] != -1)
+            {
+                x = fa[x][i];
+                y = fa[y][i];
+            }
+        }
+        x = fa[x][0];
+        y = fa[y][0];
+    }
+    return x;
+}
