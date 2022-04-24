39771185c9813ab04a6e2d67171534d5cea23c35
KIrayamoto <38139345+KIrayamoto@users.noreply.github.com>
2022-04-24 21:58:04 +0800

    Update 傅里叶变换.py

--- Python/傅里叶变换.py
+++ Python/傅里叶变换.py
@@ -1,22 +1,42 @@
+import threading
 import numpy as np
 import matplotlib.pyplot as plt
+from time import time
+y1 = []
+y2 = []
+
+
+def calc1(y, x, w0, t, M, N):
+    global y1
+    y1 = [0]*(M+1)
+    for k in range(0, M+1):
+        y1[k] = [2*np.dot(y, np.sin(k*x*w0))*t/N]
+
+
+def calc2(y, x, w0, t, M, N):
+    global y2
+    y2 = [0]*(M+1)
+    for k in range(0, M+1):
+        y2[k] = [2*np.dot(y, np.cos(k*x*w0))*t/N]
 
 
 def ft(x, y):
-    M = 1000  # 傅里叶采样个数
-    wm = 10    # 最大ω值
+    M = 5000  # 傅里叶采样个数
+    wm = 20    # 最大ω值
     N = len(x)
     t = x[-1]-x[0]
-    y1 = [0]*(2*M+1)
-    y2 = [0]*(2*M+1)
-    lk = range(-M, M+1)
     w0 = wm/M
-    for k in lk:
-        y1[k+M] = -np.dot(y, np.sin(k*x*w0))*t/N
-        y2[k+M] = np.dot(y, np.cos(k*x*w0))*t/N
-    lx = np.linspace(-wm, wm, 2*M+1)
-    plt.plot(lx, y2, color='r', label='Real')
-    plt.plot(lx, y1, color='b', label='Imaginary')
+    t0 = time()
+    thread1 = threading.Thread(target=calc1, args=(y, x, w0, t, M, N))
+    thread2 = threading.Thread(target=calc2, args=(y, x, w0, t, M, N))
+    thread1.start()
+    thread2.start()
+    thread1.join()
+    thread2.join()
+    print("Calc Time:%.4fs" % (time()-t0))
+    lx = np.linspace(0, wm, M+1)
+    plt.plot(lx, y2, color='r', label='COS')
+    plt.plot(lx, y1, color='b', label='SIN')
     plt.xlabel('ω', loc='right')
     plt.ylabel('H(ω)', loc='top', rotation=0)
     plt.legend(loc='best')
@@ -28,8 +48,8 @@ def ft(x, y):
     plt.show()
 
 
-t = 5
-N = 1000
+t = 10
+N = 5000
 x = np.linspace(-t, t, N+1)
 y0 = np.piecewise(x, [abs(x) <= 1/2, abs(x) > 1], [1, 0])
 y = np.piecewise(x, [x < 0, x >= 0], [0, lambda x: np.exp(-x)])
