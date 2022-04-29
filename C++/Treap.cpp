7a81002d1a56dd744e0a1f118a9f718122b4d751
KIrayamoto <38139345+KIrayamoto@users.noreply.github.com>
2022-04-29 20:14:54 +0800

    Create Treap.cpp

---
+++ C++/Treap.cpp
@@ -0,0 +1,184 @@
+int cnt[2 * N];
+mt19937 e;
+uniform_int_distribution<int> d(0, inf);
+struct TreeNode
+{
+	int val;
+	int size;
+	int pri;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode(int x) : val(x), size(1), pri(d(e)), left(nullptr), right(nullptr) {}
+};
+typedef pair<TreeNode*, TreeNode*> ptt;
+inline int getsize(TreeNode* root)
+{
+	return root ? root->size : 0;
+}
+inline void pu(TreeNode* root)
+{
+	if (root)
+		root->size = getsize(root->left) + getsize(root->right) + cnt[root->val + N];
+}
+ptt split(TreeNode* root, int key)
+{
+	if (root == nullptr)
+		return make_pair(nullptr, nullptr);
+	if (key < root->val)
+	{
+		ptt o = split(root->left, key);
+		root->left = o.second;
+		pu(root);
+		return make_pair(o.first, root);
+	}
+	else
+	{
+		ptt o = split(root->right, key);
+		root->right = o.first;
+		pu(root);
+		return make_pair(root, o.second);
+	}
+}
+TreeNode* merge(TreeNode* u, TreeNode* v)
+{
+	if (u == nullptr)
+		return v;
+	if (v == nullptr)
+		return u;
+	if (u->pri < v->pri)
+	{
+		u->right = merge(u->right, v);
+		pu(u);
+		return u;
+	}
+	else
+	{
+		v->left = merge(u, v->left);
+		pu(v);
+		return v;
+	}
+}
+void insert(TreeNode** root, int val)
+{
+	ptt p = split(*root, val);
+	if (cnt[val + N] == 1)
+		p.first = merge(p.first, new TreeNode(val));
+	*root = merge(p.first, p.second);
+	pu(*root);
+}
+void del(TreeNode** root, int val) {
+	if ((*root)->val == val) {
+		(*root)->size--;
+		if (cnt[val + N])
+			return;
+		int ls = getsize((*root)->left);
+		int rs = getsize((*root)->right);
+		if (!(ls || rs))
+			(*root) = nullptr;
+		else if (!ls)
+			(*root) = (*root)->right;
+		else if (!rs)
+			(*root) = (*root)->left;
+		else
+		{
+			ptt o = split(*root, val - 1);
+			ptt p = split(o.second, val);
+			*root = merge(o.first, p.second);
+		}
+	}
+	else if (val > (*root)->val)
+		del(&(*root)->right, val);
+	else
+		del(&(*root)->left, val);
+
+	pu(*root);
+}
+int findrk(TreeNode* root, int val) {
+	if (val < root->val)
+		return findrk(root->left, val);
+
+	if (val > root->val)
+		return cnt[root->val + N] + getsize(root->left) + findrk(root->right, val);
+
+	return getsize(root->left) + 1;
+}
+int findx(TreeNode* root, int val) {
+	int l = getsize(root->left);
+
+	if (val <= l)
+		return findx(root->left, val);
+
+	if (val > l + cnt[root->val + N])
+		return findx(root->right, val - l - cnt[root->val + N]);
+
+	return root->val;
+}
+int find_prev(TreeNode* root, int val) {
+	if (!root)
+		return -inf;
+
+	if (val > root->val)
+		return max(find_prev(root->right, val), root->val);
+	else
+		return find_prev(root->left, val);
+}
+int find_next(TreeNode* root, int val) {
+	if (!root)
+		return inf;
+
+	if (val >= root->val)
+		return find_next(root->right, val);
+	else
+		return min(find_next(root->left, val), root->val);
+}
+int x, y;
+TreeNode* root = nullptr;
+void solve()
+{
+	cin >> n;
+	F(0, i, n)
+	{
+		cin >> x >> y;
+		if (x == 1)
+		{
+			cnt[y + N]++;
+			insert(&root, y);
+		}
+		else if (x == 2)
+		{
+			cnt[y + N]--;
+			del(&root, y);
+		}
+		else if (x == 3)
+			cout << findrk(root, y) << "\n";
+		else if (x == 4)
+			cout << findx(root, y) << "\n";
+		else if (x == 5)
+		{
+			int j = find_prev(root, y);
+			if (j == -inf)
+				cout << "NOT FOUND\n";
+			else
+				cout << j << "\n";
+		}
+		else if (x == 6)
+		{
+			int j = find_next(root, y);
+			if (j == inf)
+				cout << "NOT FOUND\n";
+			else
+				cout << j << "\n";
+		}
+	}
+}
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	cout.setf(ios::fixed);
+	cout.precision(0);
+	//cin >> T;
+	//while (T--)
+	solve();
+	return 0;
+}
