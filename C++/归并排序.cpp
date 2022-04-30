8623188359397d98dda7ae2836db443cec07c5bf
KIrayamoto <38139345+KIrayamoto@users.noreply.github.com>
2022-04-30 17:36:45 +0800

    Update 归并排序.cpp

--- C++/归并排序.cpp
+++ C++/归并排序.cpp
@@ -1,41 +1,30 @@
-void mers(vector<int>&v)
+void mers(vector<int>& v, int x, int y)
 {
-    int n=v.size();
-    if(n==1)
-        return;
-    else
-    {
-        vector<int> v1{v.begin(),v.begin()+n/2};
-        vector<int> v2{v.begin()+n/2,v.end()};
-        int k1=v1.size(),k2=v2.size();
-        mers(v1);
-        mers(v2);
-        int l=0,r=0;
-        while(l+r<n)
-        {
-            if(l<k1&&r<k2)
-            {
-                if(v1[l]<=v2[r])
-                {
-                    v[l+r]=v1[l];
-                    ++l;
-                }
-                else
-                {
-                    v[l+r]=v2[r];
-                    ++r;
-                }
-            }
-            else if(l<k1)
-            {
-                v[l+r]=v1[l];
-                ++l;
-            }
-            else
-            {
-                v[l+r]=v2[r];
-                ++r;
-            }
-        }
-    }
+	int n = y - x;
+	if (n == 1)
+		return;
+	else
+	{
+		int k = x + n / 2;
+		mers(v, x, k);
+		mers(v, k, y);
+		vector<int>p(n);
+		int l = x, r = k;
+		F(0, i, n)
+		{
+			if (l < k && r < y)
+			{
+				if (v[l] <= v[r])
+					p[i] = v[l++];
+				else
+					p[i] = v[r++];
+			}
+			else if (l < k)
+				p[i] = v[l++];
+			else
+				p[i] = v[r++];
+		}
+		F(0, i, n)
+			v[x++] = p[i];
+	}
 }
