23519d3833ecc7854646845dcbab82d127b58cfc
KIrayamoto <38139345+KIrayamoto@users.noreply.github.com>
2022-05-11 00:57:11 +0800

    Update 归并排序.cpp

--- C++/归并排序.cpp
+++ C++/归并排序.cpp
@@ -28,3 +28,39 @@ void mers(vector<int>& v, int x, int y)
 			v[x++] = p[i];
 	}
 }
+
+// 计算逆序对
+ll mers(vector<int>&v, int x, int y)
+{
+    int n = y - x;
+    if (n == 1)
+        return 0;
+    else
+    {
+        int k = x + n / 2;
+        ll t = 0;
+        t+=mers(v, x, k)+ mers(v, k, y);
+        vector<int>p(n);
+        int l = x, r = k;
+        F(0, i, n)
+        {
+            if (l < k && r < y)
+            {
+                if (v[l] <= v[r])
+                    p[i] = v[l++];
+                else
+                {
+                    p[i] = v[r++];
+                    t += k - l;
+                }
+            }
+            else if (l < k)
+                p[i] = v[l++];
+            else
+                p[i] = v[r++];
+        }
+        F(0, i, n)
+            v[x++] = p[i];
+        return t;
+    }
+}
