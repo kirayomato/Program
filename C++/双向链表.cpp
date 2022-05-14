a0467a21ada3f15301537bb8541f7c58d3d65433
KIrayamoto <38139345+KIrayamoto@users.noreply.github.com>
2022-05-14 14:29:54 +0800

    Delete 双向链表.cpp

--- C++/双向链表.cpp
+++
@@ -1,9 +0,0 @@
-struct node
-{
-    int val;
-    node* left;
-    node* right;
-    node() :val(NULL), left(nullptr), right(nullptr) {}
-    node(int x) :val(x), left(nullptr), right(nullptr) {}
-    node(int x, node* l) :val(x), left(l), right(nullptr) {}
-};
