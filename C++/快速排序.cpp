b823b428e3353b134d3eac0c83817985f286d0f4
KIrayamoto <38139345+KIrayamoto@users.noreply.github.com>
2022-05-16 20:20:18 +0800

    Update 快速排序.cpp

--- C++/快速排序.cpp
+++ C++/快速排序.cpp
@@ -1,52 +1,17 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-vector<double> vec;
-vector<double> quicksort(vector<double> &v);
-
-int main()
+void quicksort(vector<int>&v, int x, int y)
 {
-    cout << "输入数组(回车结束)：";
-    double i;
-    while (cin >> i)
+    int n = y - x;
+    if (n <= 1)
+        return;
+    int l = x, r = y - 1, m = n >> 1;
+    vector<int>p{ v.begin() + x,v.begin() + y };
+    F(0,i,n)
     {
-        vec.push_back(i);
-        if (cin.get() == '\n')
-            break;
+        if (p[i] > p[n - 1])
+            v[r--] = p[i];
+        else
+            v[l++] = p[i];
     }
-    vec=quicksort(vec);
-    int num = vec.size();
-    cout << "排序后:";
-    for (int k = 0;k < num;k++)
-    {
-        cout << " " << vec[k];
-    }
-    return 0;
-}
-vector<double> quicksort(vector<double>&v)
-{
-    int n = v.size();
-    if(n <= 1)
-        return v;
-    else
-    {
-        vector<double> small;
-        vector<double> big;
-        int mn = n / 2;
-        double mid = v[mn];
-        for (int k = 0;k < n;k++)
-        {
-            if(k == mn)
-                continue;
-            if (v[k] > mid)
-                big.push_back(v[k]);
-            else
-                small.push_back(v[k]);
-        }
-        small = quicksort(small);
-        big = quicksort(big);
-        small.push_back(v[mn]);
-        small.insert(small.end(),big.begin(),big.end());
-        return small;
-    }
+    quicksort(v, x, l-1);
+    quicksort(v, l, y);
 }
