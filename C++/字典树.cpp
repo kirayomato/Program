4b4aa08e42a7e0aef5f9f4d8adb673647433033e
KIrayamoto <38139345+KIrayamoto@users.noreply.github.com>
2022-05-27 23:18:57 +0800

    Update 字典树.cpp

--- C++/字典树.cpp
+++ C++/字典树.cpp
@@ -1,53 +1,53 @@
 class Trie {
-    char chr;
-    unordered_map<char, Trie*> mp;
+	char chr;
+	vector<Trie*>mp = vector<Trie*>(27, nullptr);
 public:
-    Trie(char s = 0)
-    {
-        chr = s;
-    }
-    void add(string& word, int pos)
-    {
-        Trie* t;
-        if (word == "" || pos >= word.size())
-        {
-            t = new Trie();
-            mp[0] = t;
-            return;
-        }
-        if (mp[word[pos]] == nullptr)
-        {
-            t = new Trie(word[pos]);
-            mp[word[pos]] = t;
-        }
-        else
-            t = mp[word[pos]];
-        t->add(word, pos + 1);
-    }
-    void insert(string word)
-    {
-        this->add(word, 0);
-    }
-    Trie* searchprefix(string prefix, Trie* t)
-    {
-        for (char c : prefix)
-        {
-            if (t->mp[c] == nullptr)
-                return nullptr;
-            else
-                t = t->mp[c];
-        }
-        return t;
-    }
-    bool startsWith(string prefix)
-    {
-        return searchprefix(prefix, this) != nullptr;
-    }
-    bool search(string word)
-    {
-        Trie* t = searchprefix(word, this);
-        if (t != nullptr && t->mp[0] != nullptr)
-            return true;
-        return false;
-    }
+	Trie(char s = 0)
+	{
+		chr = s;
+	}
+	void add(string& word, int pos)
+	{
+		Trie* t;
+		if (word == "" || pos >= word.size())
+		{
+			t = new Trie();
+			mp[0] = t;
+			return;
+		}
+		if (mp[word[pos] - 'a' + 1] == nullptr)
+		{
+			t = new Trie(word[pos]);
+			mp[word[pos] - 'a' + 1] = t;
+		}
+		else
+			t = mp[word[pos] - 'a' + 1];
+		t->add(word, pos + 1);
+	}
+	void insert(string word)
+	{
+		this->add(word, 0);
+	}
+	Trie* searchprefix(string prefix, Trie* t)
+	{
+		for (char c : prefix)
+		{
+			if (t->mp[c - 'a' + 1] == nullptr)
+				return nullptr;
+			else
+				t = t->mp[c - 'a' + 1];
+		}
+		return t;
+	}
+	bool startsWith(string prefix)
+	{
+		return searchprefix(prefix, this) != nullptr;
+	}
+	bool search(string word)
+	{
+		Trie* t = searchprefix(word, this);
+		if (t != nullptr && t->mp[0] != nullptr)
+			return true;
+		return false;
+	}
 };
