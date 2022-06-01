4e30fb9ac16613ae62bf91657fef40759d70886e
KIrayamoto <38139345+KIrayamoto@users.noreply.github.com>
2022-06-01 10:20:25 +0800

    Update ST表.cpp

--- C++/ST表.cpp
+++ C++/ST表.cpp
@@ -1,17 +1,21 @@
-ll a[64][N];
+ll a[32][N];
+ll func(int i, int j, int k)
+{
+    return max(a[i][j], a[i][k]);
+}
 void init()
 {
-	int t = log2(n);
-	for (int i = 1;i <= t;++i)
-	{
-		int k = 1 << (i - 1);
-		int p = n - 2 * k + 1;
-		for (int j = 0;j < p;++j)
-			a[i][j] = max(a[i - 1][j], a[i - 1][j + k]);
-	}
+    int t = log2(n);
+    for (int i = 1; i <= t; ++i)
+    {
+        int k = 1 << (i - 1);
+        int p = n - 2 * k + 1;
+        for (int j = 0; j < p; ++j)
+            a[i][j] = func(i - 1, j, j + k);
+    }
 }
 ll query(int l, int r)
 {
-	int t = log2(r - l + 1);
-	return max(a[t][l - 1], a[t][r - (1 << t)]);
+    int t = log2(r - l + 1);
+    return func(t, l - 1, r - (1 << t));
 }
