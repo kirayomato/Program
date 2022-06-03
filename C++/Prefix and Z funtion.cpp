71c3b105a5bbc8092b872acca030e3be90955a7e
KIrayamoto <38139345+KIrayamoto@users.noreply.github.com>
2022-06-03 14:42:16 +0800

    Update Prefix and Z funtion.cpp

--- C++/Prefix and Z funtion.cpp
+++ C++/Prefix and Z funtion.cpp
@@ -1,8 +1,8 @@
-vector<int> prefix_function(string& s)
+int pre[N];
+void prefix_function(string& s)
 {
-    n = s.size();
-    vector<int>pre(n);
-    for(int i=1;i<n;++i)
+    int n = s.size();
+    F(0, i, n)
     {
         int j = pre[i - 1];
         while (s[i]!=s[j]&&j>0)
@@ -11,27 +11,25 @@ vector<int> prefix_function(string& s)
             j++;
         pre[i] = j;
     }
-    return pre;
 }
-vector<int> z_function(string& s)
+int z[N];
+void z_function(string s)
 {
-    n = s.size();
-    vector<int> z(n);
-    for (int i = 1, l = 0, r = 0; i < n; ++i) 
-    {
-        if (i <= r && z[i - l] < r - i + 1) 
-            z[i] = z[i - l];
-        else 
-        {
-            z[i] = max(0, r - i + 1);
-            while (i + z[i] < n && s[z[i]] == s[i + z[i]]) 
-                ++z[i];
-        }
-        if (i + z[i] - 1 > r)
-        {
-            l = i;
-            r = i + z[i] - 1;
-        }
-    }
-    return z;
+	int n = s.size(), l = 0, r = 0;
+	F(0, i, n)
+	{
+		if (i <= r && z[i - l] < r - i + 1)
+			z[i] = z[i - l];
+		else
+		{
+			z[i] = max(0, r - i + 1);
+			while (i + z[i] < n && s[z[i]] == s[i + z[i]])
+				++z[i];
+		}
+		if (i + z[i] - 1 > r)
+		{
+			l = i;
+			r = i + z[i] - 1;
+		}
+	}
 }
