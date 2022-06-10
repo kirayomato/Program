6b8f83312ea400fc6399da4d474ce9829059cfbf
KIrayamoto <38139345+KIrayamoto@users.noreply.github.com>
2022-06-10 11:07:17 +0800

    Update 回文自动机.cpp

--- C++/回文自动机.cpp
+++ C++/回文自动机.cpp
@@ -1,8 +1,8 @@
 int num, last;
 struct node
 {
-	int len, fail, cnt;
 	int ch[26] = { 0 };
+	int len, fail, cnt;
 	node() :len(0), fail(0), cnt(0) {};
 	node(int l) :len(l), fail(0), cnt(0) {};
 }pam[N];
@@ -11,32 +11,32 @@ int add(int l)
 	pam[num] = node(l);
 	return num++;
 }
-void init() 
-{  
-	add(0);add(-1);
-	pam[0].fail = 1;
-}
-int getfail(int x,int p) 
-{  
-	while (s[p - pam[x].len-1] != s[p])
+int getfail(int x, int p)
+{
+	while (s[p - pam[x].len - 1] != s[p])
 		x = pam[x].fail;
 	return x;
 }
-void insert(char c,int p) 
-{  
-	int now = getfail(last,p);
+void insert(char c, int p)
+{
+	int now = getfail(last, p);
 	if (!pam[now].ch[c - 'a'])
 	{
 		int x = add(pam[now].len + 2);
-        int t = getfail(pam[now].fail,p);
-		pam[x].fail = pam[t].ch[c - 'a'];
+		pam[x].fail = pam[getfail(pam[now].fail, p)].ch[c - 'a'];
 		pam[now].ch[c - 'a'] = x;
 	}
 	last = pam[now].ch[c - 'a'];
 	pam[last].cnt++;
 }
-void update()
+void init()
 {
+	add(0);add(-1);
+	pam[0].fail = 1;
+	s = "#" + s;
+	n = s.size();
+	F(1, i, n)
+		insert(s[i], i);
 	_F(num - 1, i, 0)
 		pam[pam[i].fail].cnt += pam[i].cnt;
 }
