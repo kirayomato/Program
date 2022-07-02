0612e1378fbfbbf0ec0c101c7239869bac04e8d5
KIrayamoto <38139345+KIrayamoto@users.noreply.github.com>
2022-07-02 19:04:27 +0800

    Update tarjan.cpp

--- C++/tarjan.cpp
+++ C++/tarjan.cpp
@@ -2,6 +2,7 @@ int dfn[N], low[N],in_st[N],sc[N],sz[N];
 vector<set<int>>G;
 stack<int> st;
 int cnt = 1, num = 1;
+// 强连通分量
 void tarjan(int u)
 {
 	dfn[u] = cnt++;
@@ -31,4 +32,28 @@ void tarjan(int u)
 		}
 		num++;
 	}
+}
+// 割点
+void tarjan(int u,int f)
+{
+	dfn[u] = cnt++;
+	low[u] = dfn[u];
+	int chi = 0;
+	FA(v, G[u])
+	{
+		if (v == f)
+			continue;
+		if (!dfn[v])
+		{
+			tarjan(v,u);
+			low[u] = min(low[u], low[v]);
+			if (u != f && low[v] >= dfn[u])
+				gd[u] = 1;
+			++chi;
+		}
+		else
+			low[u] = min(low[u], dfn[v]);
+	}
+	if (u == f && chi > 1)
+		gd[u] = 1;
 }
