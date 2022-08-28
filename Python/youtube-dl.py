0cfc5c5a6d5c068a88eb57cdf060cfa4cdad5dba
KIrayamoto <38139345+KIrayamoto@users.noreply.github.com>
2022-08-28 22:43:42 +0800

    Update youtube-dl.py

--- Python/youtube-dl.py
+++ Python/youtube-dl.py
@@ -1,14 +1,18 @@
 from os import system
 from subprocess import Popen, PIPE
 from pyperclip import copy
-a = "youtube-dl -"
-h = ' -o "D:/Download/%(title)s.%(ext)s"'
-b = " " + input("url:",)+' --proxy "socks5://127.0.0.1:10808"'
+# a = "youtube-dl -"
+a = "yt-dlp -"
+h = ' -o "D:/Download/Video/%(title)s.%(ext)s"'
+url=input("url:",)
+if "http" not in url:
+    url="https://youtu.be/"+url
+b = " " + url+' --proxy "socks5://127.0.0.1:10808"'
 cmd=a+"F"+b
 f = Popen(cmd, shell=True, stdout=PIPE, errors='replace')
 for i in f.stdout:
     print(i, end="")
-g = " " + input("download code:",)
+g = " " + input("download code:")
 cmd = a+"f"+g+b+h
 # sub = a+"-write-sub --skip-download"+b+h
 # print(sub)
@@ -17,7 +21,7 @@ cmd = a+"f"+g+b+h
 # Directly execute Version
 f = Popen(cmd, shell=True, stdout=PIPE, errors='replace')
 for i in f.stdout:
-    if 'at' in i:
+    if r'% of' in i:
         print('\r'+i.strip('\n'), flush=True, end="")
     else:
         print(i, flush=True, end="")
@@ -26,4 +30,4 @@ for i in f.stdout:
 # print(cmd)
 # copy(cmd)
 # system('start powershell')
-# a=input()
+a=input("Download Completed\n")
