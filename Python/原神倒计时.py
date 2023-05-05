1e36d4e27bce09a74f6ed77d2232808846dbee17
kirayomato <38139345+kirayomato@users.noreply.github.com>
2023-05-05 12:31:53 +0800

    Update 原神倒计时.py

--- Python/原神倒计时.py
+++ Python/原神倒计时.py
@@ -4,41 +4,29 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from time import sleep
 from warnings import filterwarnings
 import ctypes
+from sys import argv
 filterwarnings("ignore")
-address = "F:/Game/Genshin Impact/"  # Your Genshin address
-with open(address + "travel.txt", 'r') as f:
-    try:
-        flag = eval(f.readline())
-        if flag:
-            dt = datetime.strptime(f.readline(), '%Y-%m-%d %H:%M:%S\n')
-            try:
-                times = eval(f.readline())
-            except BaseException:
-                times = False
-    except BaseException:
-        flag = False
+address1 = "F:/Game/Genshin Impact/"  # Your Genshin address
+address2 = "F:/Game/Star Rail/"  # Your Genshin address
+G1 = "F:/Game/Genshin Impact/Genshin Impact Game/YuanShen.exe"
+G2 = "F:/Game/Star Rail/Game/StarRail.exe"
+dt = datetime.now()
+scheduler = BlockingScheduler()
 
 
-if not flag or times:
-    a = eval(input("hour:", ) or "9")    # default:9
-    b = eval(input("minute:", ) or "0")  # default:0
-    t = 3600 * a + 60 * b
-    dt = datetime.now() + timedelta(seconds=t)
+def start_game(address, Gaddress):
     with open(address + "travel.txt", 'w') as f:
-        f.write("True\n{}\n".format(dt.strftime('%Y-%m-%d %H:%M:%S')))
-print("end time:", dt.strftime("%Y-%m-%d %H:%M:%S"))
-
-
-def start_genshin():
+        f.write("False\n")
+        # ShellExecute(0, 'open', "C:/Users/AMD PC/Desktop/genshin.bat", '', '', 1)
     try:
-        # ShellExecute(0, 'open', address + "Genshin Impact Game/YuanShen.exe", '', '', 1)
-        ShellExecute(0, 'open', "C:/Users/AMD PC/Desktop/genshin.bat", '', '', 1)
-        with open(address + "travel.txt", 'w') as f:
-            f.write("False\n")
+        ShellExecute(0, 'open', Gaddress, '', '', 1)
     except BaseException:
-        with open(address + "travel.txt", 'a') as f:
-            f.write('True')
-
+        print("\nLaunch game failed")
+        i = input("press Enter to restart\n")
+        if i == "":
+            main()
+        else:
+            return
 
 def print_time():
     td = timedelta(seconds=(dt - datetime.now()).seconds)
@@ -46,19 +34,57 @@ def print_time():
     if td.seconds <= 0:
         scheduler.shutdown(wait=False)
         print("\rTime Out!                    ")
-        start_genshin()
+        start_game()
 
 
-if dt > datetime.now():
-    scheduler = BlockingScheduler()
-    sleep(5)  # 五秒后自动最小化窗口
-    ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 6)
-    scheduler.add_job(print_time, 'interval', seconds=1)
+def main():
+    global dt
     try:
-        scheduler.start()
-    except KeyboardInterrupt:
-        scheduler.shutdown(wait=False)
-        with open(address + "travel.txt", 'a') as f:
-            f.write('True')
-else:
-    start_genshin()
+        i = argv[1]
+    except BaseException:
+        i = input("1:Genshin\n2:Star Rail\n")
+    if i == '2':
+        addr = address2
+        Gaddr = G2
+        print("Star Rail")
+    else:
+        addr = address1
+        Gaddr = G1
+        print("Genshin")
+    with open(addr + "travel.txt", 'r') as f:
+        try:
+            flag = eval(f.readline())
+            if flag:
+                dt = datetime.strptime(f.readline(), '%Y-%m-%d %H:%M:%S\n')
+        except BaseException:
+            flag = False
+    if not flag:
+        print("input hour and minutes(format:HH MM,default 20:00)")
+        a, b = map(eval, input().split() or ('20', '0'))
+        t = 3600 * a + 60 * b
+        dt = datetime.now() + timedelta(seconds=t)
+        with open(addr + "travel.txt", 'w') as f:
+            f.write("True\n{}\n".format(dt.strftime('%Y-%m-%d %H:%M:%S')))
+    print("end time:", dt.strftime("%Y-%m-%d %H:%M:%S"))
+    if dt > datetime.now():
+        sleep(2)
+        ctypes.windll.user32.ShowWindow(
+            ctypes.windll.kernel32.GetConsoleWindow(), 6)
+        scheduler.add_job(print_time, 'interval', seconds=1)
+        try:
+            scheduler.start()
+        except BaseException:
+            print("\nProgram failed")
+            scheduler.shutdown(wait=False)
+            with open(addr + "travel.txt", 'w') as f:
+                f.write("False\n")
+            i = input("press Enter to restart\n")
+            if i == "":
+                main()
+            else:
+                return
+    else:
+        start_game(addr, Gaddr)
+
+
+main()
