017b048ecd9b5d5e084980d595f426c526f7934d
kirayomato <38139345+kirayomato@users.noreply.github.com>
2023-07-17 19:22:46 +0800

    Update 字符串高精度.cpp

--- C++/字符串高精度.cpp
+++ C++/字符串高精度.cpp
@@ -1,31 +1,24 @@
 #include<string>
 string stradd(string x, string y)//仅支持非负整数
 {
-    while (x.size() < y.size())
-        x.insert(0, "0");
-    while (x.size() > y.size())
-        y.insert(0, "0");
-    string::reverse_iterator ita, itb;
-    int t1 = 0, t2 = 0;
-    for (ita = x.rbegin(), itb = y.rbegin();ita != x.rend(), itb != y.rend();++ita, ++itb)
-    {
-        t1 = *ita + *itb - '0' - '0' + t2;
-        if (t1 >= 10)
+        if (y.size() > x.size())
+            swap(x, y);
+        int n = x.size();
+        string t = "";
+        for (int i = 0; i < n - y.size(); ++i)
+            t += "0";
+        y = t + y;
+        int t1 = 0, t2 = 0;
+        for (int i = n - 1; i >= 0; --i)
         {
-            t2 = 1;
-            t1 -= 10;
+            t1 = x[i] + y[i] - '0' - '0' + t2;
+            t2 = t1 / 10;
+            t1 %= 10;
+            x[i] = t1 + '0';
         }
-        else
-            t2 = 0;
-        *ita = t1 + '0';
-    }
-    if (t2 > 0)
-        x = char(t2 + '0') + x;
-    int i = 0, n = x.size();
-    while (x[i] == '0' && i < n - 1)
-        ++i;
-    x = x.substr(i, n);
-    return x;
+        if (t2)
+            x = char(t2 + '0') + x;
+        return x;
 }
 string strmul(string x, string y)
 {
